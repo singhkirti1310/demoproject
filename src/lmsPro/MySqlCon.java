bea27a6b9dbaccec0a73f84a6fb11f9902f52eae
singhkirti1310 <singh.kirti1310@gmail.com>
2018-09-27 01:34:23 +0530

    code sync

---
+++ src/lmsPro/MySqlCon.java
@@ -0,0 +1,32 @@
+package lmsPro;
+import java.sql.*;
+
+public class MySqlCon 
+{
+
+	public static void main(String args[])
+	{
+		try 
+		{
+		String url="jdbc:mysql://localhost:3306/lms?verifyServerCertificate=false&useSSL=false";
+		String uname="root";
+		String pass="";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,uname,pass);
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select * from books");
+		
+		while(rs.next()) {
+			String name=rs.getString("author");
+			System.out.println(name);
+		}
+		
+		stmt.close();
+		con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
